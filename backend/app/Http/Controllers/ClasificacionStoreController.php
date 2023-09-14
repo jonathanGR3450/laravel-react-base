5b6c5f9f73cb1f5db2bd13309bcc4913bbf4ffd2
jonathan Garzon <jonatangarzon95@gmail.com>
2023-09-14 10:35:15 -0500

    feat: save calificacion unidad construccion

--- backend/app/Http/Controllers/ClasificacionStoreController.php
+++ backend/app/Http/Controllers/ClasificacionStoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ValidarConstruccionesFormRequest;
-use Illuminate\Http\Request;
+use App\Models\Local\LcCaracteristicasUnidadConstruccionLocal;
 
 class ClasificacionStoreController extends Controller
 {
@@ -12,6 +12,62 @@ class ClasificacionStoreController extends Controller
      */
     public function __invoke(ValidarConstruccionesFormRequest $request)
     {
-        dd($request->validated());
+        // dd($request->validated());
+        $construcciones = $request->input('construcciones', []);
+        foreach ($construcciones as $construccion) {
+            $caracteristicas = (object) $construccion['caracteristicasunidadconstruccion'];
+            
+            $unidadConstruccionModel = LcCaracteristicasUnidadConstruccionLocal::create([
+                'identificador' => $caracteristicas->identificador,
+                'tipo_construccion' => $caracteristicas->tipo_construccion,
+                'tipo_unidad_construccion' => $caracteristicas->tipo_unidad_construccion,
+                'tipo_planta' => $caracteristicas->tipo_planta,
+                'total_plantas' => $caracteristicas->total_plantas,
+                'total_habitaciones' => $caracteristicas->total_habitaciones,
+                'total_banios' => $caracteristicas->total_banios,
+                'total_locales' => $caracteristicas->total_locales,
+                'anio_construccion' => $caracteristicas->anio_construccion,
+                'uso' => $caracteristicas->uso,
+                'avaluo_unidad_construccion' => $caracteristicas->avaluo_unidad_construccion,
+                'area_construida' => $caracteristicas->area_construida,
+                'area_privada_construida' => $caracteristicas->area_privada_construida,
+                'comienzo_vida_util_version' => $caracteristicas->comienzo_vida_util_version,
+                'fin_vida_util_version' => $caracteristicas->fin_vida_util_version,
+                'espacio_de_nombres' => $caracteristicas->espacio_de_nombres,
+                'local_id' => $caracteristicas->local_id,
+                'observaciones' => $caracteristicas->observaciones,
+            ]);
+
+            foreach ($caracteristicas->calificacionconvencional as $calificacion) {
+                $calificacion = (object) $calificacion;
+                $calificacionConvencionalModel = $unidadConstruccionModel->calificacionConvencional()->create([
+                    'tipo_calificar' => $calificacion->tipo_calificar,
+                    'total_calificacion' => $calificacion->total_calificacion,
+                ]);
+
+                foreach ($calificacion->grupocalificacion as $grupo) {
+                    $grupo = (object) $grupo;
+                    $grupoCalificacionModel = $calificacionConvencionalModel->grupoCalificacion()->create([
+                        'clase_calificacion' => $grupo->clase_calificacion,
+                        'conservacion' => $grupo->conservacion,
+                        'subtotal' => $grupo->subtotal,
+                    ]);
+
+                    foreach ($grupo->objetoconstruccion as $objeto) {
+                        $objeto = (object) $objeto;
+                        $objetoModel = $grupoCalificacionModel->objetoConstruccion()->create([
+                            'tipo_objeto_construccion' => $objeto->tipo_objeto_construccion,
+                            'puntos' => $objeto->puntos,
+                        ]);
+                    }
+                }
+            }
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Calificacion creada con exito',
+            'data' => null
+        ]);
     }
 }
