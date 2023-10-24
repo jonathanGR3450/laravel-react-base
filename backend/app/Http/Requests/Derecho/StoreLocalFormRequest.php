b5c96ceac4a3dd4b25c3431db4c96d25f5cdb520
jonathan Garzon <jonatangarzon95@gmail.com>
2023-10-24 09:19:21 -0500

    feat: store derecho

---
+++ backend/app/Http/Requests/Derecho/StoreLocalFormRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Derecho;
+
+use App\Traits\ValidationErrorResponseTrait;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLocalFormRequest extends FormRequest
+{
+    use ValidationErrorResponseTrait;
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tipo' => 'required|exists:pgsqlcatastro.lc_derechotipo,t_id',
+            'fraccion_derecho' => 'required|numeric|between:0,1',
+            'fecha_inicio_tenencia' => 'nullable|date',
+            'descripcion' => 'nullable|string|max:255',
+            'interesado_lc_interesado' => 'required|exists:lc_interesado,t_id',
+            'interesado_lc_agrupacioninteresados' => 'nullable|exists:lc_agrupacioninteresados,t_id',
+            'unidad' => 'nullable|exists:lc_predio,t_id',
+            'comienzo_vida_util_version' => 'required|date',
+            'fin_vida_util_version' => 'nullable|date|after:comienzo_vida_util_version',
+            'espacio_de_nombres' => 'required|string|max:255',
+        ];
+    }
+}
