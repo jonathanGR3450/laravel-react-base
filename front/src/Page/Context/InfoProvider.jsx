7a2cc92c18fcf8f1441be9f4aa9390c441b12029
lcarlos <luiscardozoicg@gmail.com>
2023-11-20 15:37:59 -0500

    Add alert to create sucesfull resolution

--- front/src/Page/Context/InfoProvider.jsx
+++ front/src/Page/Context/InfoProvider.jsx
@@ -1,11 +1,14 @@
 import { useState, createContext, useEffect } from "react";
 import clienteAxios from "../../config/clienteAxios";
+import { useNavigate } from "react-router-dom";
 
 const InfoContext = createContext();
 
 const InfoProvider = ({ children }) => {
   const [infoResolucion, setInfoResolucion] = useState([]);
   const [alerta, setAlerta] = useState([]);
+
+  const navigate = useNavigate();
 
   const mostrarAlerta = (alerta) => {
     setAlerta(alerta);
@@ -28,7 +31,15 @@ const InfoProvider = ({ children }) => {
         config
       );
       console.log(data);
-      
+
+      setAlerta({
+        msg: "Resolución Creada Correctamente",
+        error: false,
+      });
+      setTimeout(()=>{
+          setAlerta({})
+          navigate("/resoluciones")
+      }, 3000)
     } catch (error) {
       console.log(error);
     }
