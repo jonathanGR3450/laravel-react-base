ff962b317ae85f4d816cf8e78f7111c53650ef53
lcarlos <luiscardozoicg@gmail.com>
2023-12-03 16:17:07 -0500

    Ajustes FormularioResolucion

--- front/src/components/TablaResoluciones.jsx
+++ front/src/components/TablaResoluciones.jsx
@@ -1,15 +1,30 @@
+import useInfo from "../hooks/useInfo";
+
 const TablaResoluciones = () => {
+  const { resultado } = useInfo();
+  console.log(resultado);
+  const { data: info } = resultado;
+  const { radicado } = info;
+  console.log(radicado);
+  const { t_id, url, no_radicado, tramite_id } = radicado;
   const data = [
-    { radicado: 'R-2023-1234', idAsociado: "ID-12345", resolucion: ' 03-001-000', url: "http://localhost/storage/documents/2023-11-20-1973e4fa-18fc-4d57-817a-371f9cd80337-document.pdf" }
+    {
+      id: t_id,
+      radicado: no_radicado,
+      idAsociado: "ID-12345",
+      resolucion: " 03-001-000",
+      url: url,
+    },
   ];
 
   const openPdfInNewTab = (url) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
   return (
     <table className="min-w-full bg-white border border-gray-300">
       <thead>
         <tr>
+          <th className="py-2 px-4 border-b">Id</th>
           <th className="py-2 px-4 border-b">Radicado</th>
           <th className="py-2 px-4 border-b">ID Asociado</th>
           <th className="py-2 px-4 border-b">Resolución</th>
@@ -19,6 +34,7 @@ const TablaResoluciones = () => {
       <tbody>
         {data.map((item, index) => (
           <tr key={index}>
+            <td className="py-2 px-4 border-b">{item.id}</td>
             <td className="py-2 px-4 border-b">{item.radicado}</td>
             <td className="py-2 px-4 border-b">{item.idAsociado}</td>
             <td className="py-2 px-4 border-b">{item.resolucion}</td>
