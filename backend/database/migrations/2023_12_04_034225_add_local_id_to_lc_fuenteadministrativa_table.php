311ba53a07837c8b5056988a3b054a47c1d98a9c
jonathan Garzon <jonatangarzon95@gmail.com>
2023-12-04 20:43:55 -0500

    fix local id

---
+++ backend/database/migrations/2023_12_04_034225_add_local_id_to_lc_fuenteadministrativa_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lc_fuenteadministrativa', function (Blueprint $table) {
+            $table->string('local_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lc_fuenteadministrativa', function (Blueprint $table) {
+            $table->dropColumn('local_id');
+        });
+    }
+};
