8ecf4f08d8a0985892e1f9fd04dc8009e1f4efec
gearwell <gearwell@hotmail.com>
2023-12-07 02:40:05 -0500

    Login Back

--- front/src/Page/Login.jsx
+++ front/src/Page/Login.jsx
@@ -1,32 +1,31 @@
 import { useEffect, useState } from "react";
+import  Cookies  from "universal-cookie";
 import ZonaGeo from "../Json/zonaGeoEconomica.json";
 import TablaCatastral from "../Json/prueba.json";
 import TablaVivienda from "../Json/vivienda.json";
 import Loader from "./Loader";
+import { useNavigate } from 'react-router-dom';
 const LoginForm = () => {
   
-
+    const navigate = useNavigate();
     
-    const [vigenciaTipo, setVigenciaTipo] = useState(["2023", "2024", "2025"])
+    
     const [jsonValues,setJsonValues] = useState(  
       {
-        incremento:0,
-        decreto:"Na",
-        vigencia:"2023"
+        email:"aa@a.com",
+        password:"password"
       }
     )
+    const cookies = new Cookies();
     
     
     
-    const Vigencia = vigenciaTipo.map(Vigencia => Vigencia )
-    
-    async function postJSON(data,token) {
+    async function postJSON(data) {
       try {
-        const response = await fetch("http://localhost/api/v1/avaluo-catastral/calcular/incremento", {
+        const response = await fetch("http://localhost/api/login", {
           method: "POST", // or 'PUT'
           headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + token
+            "Content-Type": "application/json"
           },
           body: JSON.stringify(data),
           
@@ -34,6 +33,14 @@ const LoginForm = () => {
     
         const result = await response.json();
         console.log("Success:", result);
+        console.log("Success:", result.authorization.token);
+        const today = new Date();
+        cookies.set('tk', result.authorization.token, { path: '/',sameSite:"lax" });
+        cookies.set('loginDate', today);
+        console.log("Fecha Inicio de Sesion: ");
+        console.log(today);
+        console.log("Token Log Cookie:", cookies.get('tk'));
+        navigate(-1)
       } catch (error) {
         console.error("Error:", error);
       }
@@ -46,37 +53,27 @@ const LoginForm = () => {
     }
     function handleEnviar(e) {    
       console.log("Enviando ...");
-      const jsonEnviar={"vigencia":jsonValues.vigencia,"incremento":jsonValues.incremento,"tablas": ["tab_anexos_urbana_rural"]}
+      console.log(cookies)
+      const jsonEnviar={"email":jsonValues.email,"password":jsonValues.password}
       console.log(jsonEnviar)
       console.log(jsonValues)
-      postJSON(jsonEnviar,"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2FwaS9sb2dpbiIsImlhdCI6MTcwMTA1Njc2NSwiZXhwIjoxNzAxMDYwMzY1LCJuYmYiOjE3MDEwNTY3NjUsImp0aSI6Ikw2SUQ3aTNHVHVBY05sNHAiLCJzdWIiOiIzIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.vJ9u8oDAHOd_pbL-pOROF2NQfrXIt1kj3ltGnoPBoPI");
+      postJSON(jsonEnviar);
     }
     function handleInput(e) {    
       
       const {name, value} = e.target;
-      jsonValues.incremento=value
+      jsonValues.email=value
       console.log(value);
       
     }
     function handleInput2(e) {    
       
       const {name, value} = e.target;
-      jsonValues.decreto=value
+      jsonValues.password=value
       console.log(value);
       
     }    
-    const sel=(
-      <>
-        < select
-        onChange={e => handleVigenciaTipoChange(e)}
-        className="border-2 p-2 w-mid rounded-lg text-center " >
-        {
-          Vigencia.map((address, key) => <option value={key}>{address}</option>)
-        }
-        </select >
-        <input type="submit" value="Submit" />
-      </>
-      );
+
     return (
       
       <>
@@ -85,7 +82,7 @@ const LoginForm = () => {
             <label className="w-mid font-semibold">Usuario : </label>
             <input
               onChange={handleInput}
-              type="text"
+              type="email"
               className="border-2 p-2 rounded-lg text-center w-full"
               name="area"
               hint="d0001"
@@ -95,22 +92,15 @@ const LoginForm = () => {
             <label className="w-mid font-semibold">Contraseña : </label>
             <input
               onChange={handleInput2}
-              type="text"
+              type="password"
               className="border-2 p-2 rounded-lg text-center w-full"
+              value="password"
               name="area"
               hint="d0001"
             ></input>
         </div>
         <br/>
-        <label className="w-mid font-semibold">Vigencia : </label>
-        < select
-        onChange={e => handleVigenciaTipoChange(e)}
-        defaultValue="2023"
-        className="border-2 p-2 w-mid rounded-lg text-center " >
-        {
-          Vigencia.map((address, key) => <option value={key}>{address}</option>)
-        }
-        </select >
+
         <br/>
         <div className="w-full flex flex-col items-center justify-center mt-4">
           <button className="p-2 w-1/4 text-center  rounded-md  border-2  text-white bg-teal-500 "
